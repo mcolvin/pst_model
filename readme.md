fe425a047b0181582a103209c49e8bb8f6fe0f4a
Michael Colvin <colvin.mike@gmail.com>
2014-03-27 14:54:21 -0700

    updated source code and readme
    Added stage specific functions to simulate daily dynamics given
    survival.  Added information to readme file.

--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
+Pallid Sturgeon Effects Analysis Quantitative Modeling
+===============================================
+
+# Objectives
+
+# Description
 This repository contains source code for effects analysis of Missouri River hydro operations on Pallid Sturgeon population dynamics.  
+
+# Spatial extent and grain
+The model simulates Missouri River Pallid sturgeon populations from the confluence of the Missouri River with the Mississippi River upstream to the Missouri and Yellowstone River reaches in Montana, USA.  The Missouri River is divided into 2 major basin, lower and upper representing natural breaks in the river system and Pallid sturgeon population.  The upper and lower sections are divided into 2 lower and upper sections.  
+
+* Lower Missouri River basin (RKM 0 to RKM ??)
+    * Lower basin-Lower Missouri River segment (RKM ?? to RKM ??))
+	* Upperbasin-Lower Missouri River segment (RKM ?? to RKM ??))
+* Upper Missouri River basin (RKM 0 to RKM ??)
+    * Upper basin-Lower Missouri River segment (RKM ?? to RKM ??))
+	* Lower basin-Lower Missouri River segment (RKM ?? to RKM ??))
+	
+# Temporal resolution
+The model simulates population dynamics on a daily time step.
+
+# Pallid sturgeon life history and stages
---
+++ src/functions.R
@@ -0,0 +1,71 @@
+
+
+
+eggs<- function(val, surv, stoch=FALSE,...)
+	{
+	# INPUTS
+	# val: NUMBER OF EGGS AT PREVIOUS DAY
+	# surv: DAILY SURVIVAL RATE TO APPLY TO EGGS
+	# stoch: SHOULD OUTPUT BE STOCHASTIC?  DEFAULTS TO FALSE
+	# eggs_out: THIS IS THE VALUE RETURNED BY THE FUNCTION
+	
+	eggs<- val
+	eggs_out<- ifelse(stoch==FALSE, eggs*surv, rpois(eggs*surv))
+	return(eggs_out)	
+	}
+	
+embryo<- function(val, surv, stoch=FALSE,...)
+	{
+	# INPUTS
+	# val: NUMBER OF EMBRYOS AT PREVIOUS DAY
+	# surv: DAILY SURVIVAL RATE TO APPLY TO EMBRYOS
+	# stoch: SHOULD OUTPUT BE STOCHASTIC?  DEFAULTS TO FALSE
+	# embryo_out: THIS IS THE VALUE RETURNED BY THE FUNCTION
+	
+	embryo<- val
+	embryo_out<- ifelse(stoch==FALSE, embryo*surv, rpois(embryo*surv))
+	return(embryo_out)	
+	}
+
+exogenously_feeding_embryo<- function(val, surv, stoch=FALSE,...)
+	{
+	# INPUTS
+	# val: NUMBER OF EXOGENOUSLY FEEDING EMBRYOS AT PREVIOUS DAY
+	# surv: DAILY SURVIVAL RATE TO APPLY TO EXOGENOUSLY FEEDING 
+	# stoch: SHOULD OUTPUT BE STOCHASTIC?  DEFAULTS TO FALSE
+	# ef_embryo_out: THIS IS THE VALUE RETURNED BY THE FUNCTION
+	
+	ef_embryo<- val
+	ef_embryo_out<- ifelse(stoch==FALSE, ef_embryo*surv, rpois(ef_embryo*surv))
+	return(ef_embryo_out)	
+	}
+	
+juveniles<- function(val, surv, stoch=FALSE,...)
+	{
+	# INPUTS
+	# val: NUMBER OF JUVENILES AT PREVIOUS DAY
+	# surv: DAILY SURVIVAL RATE TO APPLY TO JUVENILES
+	# stoch: SHOULD OUTPUT BE STOCHASTIC?  DEFAULTS TO FALSE
+	# juvenile_out: THIS IS THE VALUE RETURNED BY THE FUNCTION
+	
+	juvenile<- val
+	juvenile_out<- ifelse(stoch==FALSE, juvenile*surv, rpois(juvenile*surv))
+	return(juvenile_out)	
+	}	
+	
+adults<- function(val, surv, stoch=FALSE,...)
+	{
+	# INPUTS
+	# val: NUMBER OF ADULTS AT PREVIOUS DAY
+	# surv: DAILY SURVIVAL RATE TO APPLY TO ADULTS
+	# stoch: SHOULD OUTPUT BE STOCHASTIC?  DEFAULTS TO FALSE
+	# adult_out: THIS IS THE VALUE RETURNED BY THE FUNCTION
+	
+	adult<- val
+	adult_out<- ifelse(stoch==FALSE, adult*surv, rpois(adult*surv))
+	return(adult_out)	
+	}	
+	
+	
+	
+	
